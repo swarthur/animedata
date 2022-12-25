20097506e406289c85f90a3325c7b2d91dcd00f2
arthur <arthur.cott@free.fr>
2022-12-25 21:20:53 +0100

    Testing for check_dict and corrections.

--- ad_test.py
+++ ad_test.py
@@ -2,7 +2,11 @@ import animedata as ad
 
 if __name__ == "__main__":
     ad.dev_mode = True
-    #ad.update_anime_lib()
-    json_dict = ad.load_json_dict(True)
-    print(json_dict)
+    ad.update_anime_lib()
+    json_dict = ad.load_json(True)
     ad.save_json(json_dict)
+    json_dict_2 = ad.load_json(False)
+    print(ad.check_dict(json_dict))
+    print(ad.check_dict(json_dict_2))
+    if json_dict != json_dict_2:
+        print("Something went wrong ...")
